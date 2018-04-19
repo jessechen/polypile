c4f6b530270ee1c83ccc4fb6309a961d0a22089e
Jesse Chen <jesse.z.chen@gmail.com>
2018-04-18 20:40:36 -0400

    Draw three blue hexagons.

--- polypile.js
+++ polypile.js
@@ -1,7 +1,7 @@
 const TAU = 2 * Math.PI;
 
 let canvasWidth, canvasHeight;
-let shapes = [];
+let tile = [];
 
 function setup() {
     canvasWidth = windowWidth;
@@ -12,9 +12,9 @@ function setup() {
 }
 
 function draw() {
-    stroke(color(0,15,85));
+    stroke(color(0, 15, 85));
     noFill();
-    for (shape of shapes) {
+    for (shape of tile) {
         beginShape();
         for (p of shape.points) {
             vertex(p.x, p.y);
@@ -24,20 +24,18 @@ function draw() {
 }
 
 class Shape {
-    constructor(x0, y0, sides, sideLength) {
-        this.x0 = x0;
-        this.y0 = y0;
+    constructor(initialPoint, sides, sideLength) {
         this.sides = sides;
         this.sideLength = sideLength;
-        this.points = [new Point(this.x0, this.y0)];
+        this.points = [initialPoint];
 
         let angle = 0;
         for (let i = 0; i < this.sides; i++) {
-            angle += TAU / this.sides;
             let p = this.points[this.points.length - 1];
             let dx = Math.cos(angle) * this.sideLength;
             let dy = Math.sin(angle) * this.sideLength;
             this.points.push(new Point(p.x + dx, p.y + dy));
+            angle += TAU / this.sides;
         }
     }
 }
@@ -49,6 +47,7 @@ class Point {
     }
 }
 
-shapes = [
-    new Shape(100, 100, 6, 50)
-];
+tile = [];
+tile.push(new Shape(new Point(25, 25), 6, 50));
+tile.push(new Shape(tile[0].points[2], 6, 50));
+tile.push(new Shape(tile[0].points[4], 6, 50));
